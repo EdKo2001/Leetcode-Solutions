0c37e3b202c79543cb109ec8aeb4d1986415d5ed
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-09-20 19:24:04 -0500

    Time: 4 ms (65.40%) | Memory: 47.9 MB (92.20%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,20 +1,38 @@
 class Solution {
+    // public List<Integer> lexicalOrder(int n) {
+    // List<String> nums = new ArrayList<>();
+
+    // while (n > 0) {
+    // nums.add(String.valueOf(n));
+    // n--;
+    // }
+
+    // Collections.sort(nums);
+
+    // List<Integer> lexNums = new ArrayList<>();
+
+    // for (String num : nums) {
+    // lexNums.add(Integer.parseInt(num));
+    // }
+
+    // return lexNums;
+    // }
+
     public List<Integer> lexicalOrder(int n) {
-        List<String> nums = new ArrayList<>();
-
-        while (n > 0) {
-            nums.add(String.valueOf(n));
-            n--;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            dfs(i, n, result);
         }
+        return result;
+    }
 
-        Collections.sort(nums);
-
-        List<Integer> lexNums = new ArrayList<>();
-
-        for (String num : nums) {
-            lexNums.add(Integer.parseInt(num));
+    private void dfs(int current, int n, List<Integer> result) {
+        if (current > n) {
+            return;
         }
-
-        return lexNums;
+        result.add(current);
+        for (int i = 0; i <= 9; i++) {
+            dfs(current * 10 + i, n, result);
+        }
     }
 }
