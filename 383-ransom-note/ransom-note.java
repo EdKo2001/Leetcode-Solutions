22e6549afaa5a656e32e8127978f04dd23d719e2
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-09-26 14:44:27 -0500

    Time: 1 ms (99.17%) | Memory: 44.8 MB (57.74%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,26 +1,42 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character, Integer> dictionary = new HashMap<>();
+
+//         for (int i = 0; i < magazine.length(); i++) {
+//             char c = magazine.charAt(i);
+
+//             int currentCount = dictionary.getOrDefault(c, 0);
+
+//             dictionary.put(c, ++currentCount);
+//         }
+
+//         for (int i = 0; i < ransomNote.length(); i++) {
+//             char c = ransomNote.charAt(i);
+
+//             int currentCount = dictionary.getOrDefault(c, 0);
+
+//             if (currentCount == 0)
+//                 return false;
+
+//             dictionary.put(c, --currentCount);
+//         }
+
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> dictionary = new HashMap<>();
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-        for (int i = 0; i < magazine.length(); i++) {
-            char c = magazine.charAt(i);
-
-            int currentCount = dictionary.getOrDefault(c, 0);
-
-            dictionary.put(c, ++currentCount);
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
-
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char c = ransomNote.charAt(i);
-
-            int currentCount = dictionary.getOrDefault(c, 0);
-
-            if (currentCount == 0)
-                return false;
-
-            dictionary.put(c, --currentCount);
-        }
-
         return true;
     }
 }
