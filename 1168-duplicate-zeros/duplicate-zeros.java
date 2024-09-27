048582c489d56d11d8fa8ea72491dc67cbc2db58
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-09-27 10:17:24 -0500

    Time: 14 ms (24.32%) | Memory: 45 MB (6.49%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public void duplicateZeros(int[] arr) {
+//         int zeros = 0;
+//         int n = arr.length;
+
+//         // Count the number of zeros
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == 0) {
+//                 zeros++;
+//             }
+//         }
+
+//         // Traverse the array backwards and shift elements accordingly
+//         for (int i = n - 1, j = n + zeros - 1; i >= 0; i--, j--) {
+//             if (j < n) {
+//                 arr[j] = arr[i];
+//             }
+//             if (arr[i] == 0 && --j < n) {
+//                 arr[j] = 0;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int zeros = 0;
-        int n = arr.length;
-
-        // Count the number of zeros
+       int n = arr.length;
+        
+        // Traverse the array
         for (int i = 0; i < n; i++) {
+            // If a zero is encountered
             if (arr[i] == 0) {
-                zeros++;
-            }
-        }
-
-        // Traverse the array backwards and shift elements accordingly
-        for (int i = n - 1, j = n + zeros - 1; i >= 0; i--, j--) {
-            if (j < n) {
-                arr[j] = arr[i];
-            }
-            if (arr[i] == 0 && --j < n) {
-                arr[j] = 0;
+                // Shift elements to the right, starting from the end
+                for (int j = n - 1; j > i; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                // Duplicate the zero by setting the next element to zero
+                i++;  // Skip over the next position as it contains the duplicated zero
             }
         }
     }
