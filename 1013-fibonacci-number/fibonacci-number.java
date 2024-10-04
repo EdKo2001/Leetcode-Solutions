1d28731dfe7afacdb05a66617d79f921d001451d
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-04 14:59:23 -0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (7.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,15 @@
 class Solution {
+    int[] cache = new int[10001];
+
     public int fib(int n) {
         if (n < 2)
             return n;
 
-        return fib(n - 1) + fib(n - 2);
+        if (cache[n] != 0)
+            return cache[n];
+
+        cache[n] = fib(n - 1) + fib(n - 2);
+
+        return cache[n];
     }
 }
