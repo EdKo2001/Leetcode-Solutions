2b2b929685f3ea2289b4824553bd93b4d2e7cb3a
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-04 15:07:11 -0500

    Time: 0 ms (100.00%) | Memory: 40 MB (80.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,30 @@
 class Solution {
-    int[] cache = new int[10001];
+    // Top-Down Dynamic Programming
+    // int[] cache = new int[10001];
 
+    // public int fib(int n) {
+    // if (n < 2)
+    // return n;
+
+    // if (cache[n] != 0)
+    // return cache[n];
+
+    // cache[n] = fib(n - 1) + fib(n - 2);
+
+    // return cache[n];
+    // }
+
+    // Down-Up Dynamic Programming
     public int fib(int n) {
-        if (n < 2)
+        if (n == 0 || n == 1)
             return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-        if (cache[n] != 0)
-            return cache[n];
-
-        cache[n] = fib(n - 1) + fib(n - 2);
-
-        return cache[n];
+        return dp[n];
     }
 }
