ed17fbef621a67c1f19d9be9df7df2f24aca3808
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-17 08:15:31 -0500

    Time: 21 ms (51.57%) | Memory: 49.8 MB (72.82%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -3,16 +3,16 @@ class KthLargest {
     // List<Integer> stream = new ArrayList<>();
 
     // public KthLargest(int k, int[] nums) {
-    //     this.k = k;
-    //     for(int i = 0; i < nums.length; i++) {
-    //         stream.add(nums[i]);
-    //     }
+    // this.k = k;
+    // for(int i = 0; i < nums.length; i++) {
+    // stream.add(nums[i]);
     // }
-    
+    // }
+
     // public int add(int val) {
-    //     stream.add(val);
-    //     Collections.sort(stream, Comparator.reverseOrder());
-    //     return stream.get(k - 1);
+    // stream.add(val);
+    // Collections.sort(stream, Comparator.reverseOrder());
+    // return stream.get(k - 1);
     // }
 
     private PriorityQueue<Integer> minHeap;
@@ -27,15 +27,23 @@ class KthLargest {
     }
 
     public int add(int val) {
-        minHeap.offer(val);
-        if (minHeap.size() > k) {
-            minHeap.poll();
+        // minHeap.offer(val);
+        // if (minHeap.size() > k) {
+        // minHeap.poll();
+        // }
+        // return minHeap.peek();
+
+        // Add to our minHeap if we haven't processed k elements yet
+        // or if val is greater than the top element (the k-th largest)
+        if (minHeap.size() < k || minHeap.peek() < val) {
+            minHeap.add(val);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
         }
         return minHeap.peek();
     }
 }
-
-
 
 /**
  * Your KthLargest object will be instantiated and called as such:
