e8a4732a68f790463b0be38ceaed2f50463d2d74
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-21 21:55:12 -0500

    Time: 17 ms (98.45%) | Memory: 65.3 MB (22.60%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -14,51 +14,91 @@
  * }
  */
 class Solution {
+    // Explicit lvl counting
+    // public long kthLargestLevelSum(TreeNode root, int k) {
+    //     if (root == null)
+    //         return -1;
+
+    //     PriorityQueue<Long> minHeap = new PriorityQueue<>();
+    //     long curSum = 0;
+
+    //     Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+    //     queue.offer(new Pair<>(root, 0));
+
+    //     int curLvl = 0;
+    //     while (!queue.isEmpty()) {
+    //         Pair<TreeNode, Integer> current = queue.poll();
+    //         TreeNode currentNode = current.getKey();
+    //         int level = current.getValue();
+
+    //         if (level > curLvl) {
+    //             if (minHeap.size() < k || minHeap.peek() < curSum) {
+    //                 minHeap.offer(curSum);
+    //                 if (minHeap.size() > k) {
+    //                     minHeap.poll();
+    //                 }
+    //             }
+    //             curSum = 0;
+    //             curLvl = level;
+    //         }
+
+    //         curSum += currentNode.val;
+
+    //         if (currentNode.left != null)
+    //             queue.offer(new Pair<>(currentNode.left, level + 1));
+
+    //         if (currentNode.right != null)
+    //             queue.offer(new Pair<>(currentNode.right, level + 1));
+    //     }
+
+    //     // Add the sum of the last level
+    //     if (minHeap.size() < k || minHeap.peek() < curSum) {
+    //         minHeap.offer(curSum);
+    //         if (minHeap.size() > k) {
+    //             minHeap.poll();
+    //         }
+    //     }
+
+    //     // If we don't have k levels, return 0 or handle appropriately
+    //     return minHeap.size() == k ? minHeap.peek() : -1;
+    // }
+
     public long kthLargestLevelSum(TreeNode root, int k) {
-        if (root == null)
+        if (root == null) {
             return -1;
+        }
 
         PriorityQueue<Long> minHeap = new PriorityQueue<>();
-        long curSum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
 
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        queue.offer(new Pair<>(root, 0));
-
-        int curLvl = 0;
         while (!queue.isEmpty()) {
-            Pair<TreeNode, Integer> current = queue.poll();
-            TreeNode currentNode = current.getKey();
-            int level = current.getValue();
+            int size = queue.size(); // Get the number of nodes at the current level
+            long curSum = 0;
 
-            if (level > curLvl) {
-                if (minHeap.size() < k || minHeap.peek() < curSum) {
-                    minHeap.offer(curSum);
-                    if (minHeap.size() > k) {
-                        minHeap.poll();
-                    }
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+                curSum += currentNode.val; // Accumulate the sum of the current level
+
+                // Add child nodes for the next level
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
                 }
-                curSum = 0;
-                curLvl = level;
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
             }
 
-            curSum += currentNode.val;
-
-            if (currentNode.left != null)
-                queue.offer(new Pair<>(currentNode.left, level + 1));
-
-            if (currentNode.right != null)
-                queue.offer(new Pair<>(currentNode.right, level + 1));
-        }
-
-        // Add the sum of the last level
-        if (minHeap.size() < k || minHeap.peek() < curSum) {
-            minHeap.offer(curSum);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+            // Add the current level's sum to the min-heap
+            if (minHeap.size() < k) {
+                minHeap.offer(curSum);
+            } else if (curSum > minHeap.peek()) {
+                minHeap.poll(); // Remove the smallest sum
+                minHeap.offer(curSum); // Add the new level sum
             }
         }
 
-        // If we don't have k levels, return 0 or handle appropriately
+        // If there are fewer than k levels, return -1
         return minHeap.size() == k ? minHeap.peek() : -1;
     }
 }
