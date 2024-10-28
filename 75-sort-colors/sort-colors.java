5df8148e0d5f58be56537830873b525917021495
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-28 13:55:16 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (66.10%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,23 +1,50 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int redIdx = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                swap(nums, redIdx++, i);
-            }
-        }
+    // Solution #1 (Two Pass)
+    // public void sortColors(int[] nums) {
+    //     int redIdx = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (nums[i] == 0) {
+    //             swap(nums, redIdx++, i);
+    //         }
+    //     }
 
-        int whiteIdx = redIdx;
-        for (int i = redIdx; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                swap(nums, whiteIdx++, i);
+    //     int whiteIdx = redIdx;
+    //     for (int i = redIdx; i < nums.length; i++) {
+    //         if (nums[i] == 1) {
+    //             swap(nums, whiteIdx++, i);
+    //         }
+    //     }
+    // }
+
+    // private void swap(int[] nums, int idx1, int idx2) {
+    //     int tmp = nums[idx1];
+    //     nums[idx1] = nums[idx2];
+    //     nums[idx2] = tmp;
+    // }
+
+    // Solution #2 (One Pass => Dutch National Flag algorithm)
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
             }
         }
     }
 
-    public static void swap(int[] nums, int idx1, int idx2) {
-        int tmp = nums[idx1];
+    private void swap(int[] nums, int idx1, int idx2) {
+        int temp = nums[idx1];
         nums[idx1] = nums[idx2];
-        nums[idx2] = tmp;
+        nums[idx2] = temp;
     }
 }
