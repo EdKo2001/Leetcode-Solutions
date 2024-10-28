378cdc591347c4bb1d0080d582fe3448109395df
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-10-28 10:49:57 -0500

    Time: 0 ms (100.00%) | Memory: 40.7 MB (76.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,26 +1,62 @@
 class Solution {
+    // Solutin #1 (Brute Force)
+    // Time Complexity: O(n^2)
+    // Spae Complexity: O(n)
+    // public int rob(int[] nums) {
+    //     int N = nums.length;
+    //     if (N == 1)
+    //         return nums[0];
+    //     if (N == 2)
+    //         return Math.max(nums[0], nums[1]);
+
+    //     int max = Math.max(nums[0], nums[1]);
+    //     int[] dp = new int[N];
+
+    //     dp[0] = nums[0];
+    //     dp[1] = nums[1];
+
+    //     for (int i = 2; i < N; i++) {
+    //         for (int j = 0; j < i - 1; j++) {
+    //             int curSum = dp[j] + nums[i];
+    //             if (curSum > dp[i]) {
+    //                 dp[i] = curSum;
+    //                 max = Math.max(max, curSum);
+    //             }
+    //         }
+    //     }
+
+    //     return max;
+    // }
+
+    // Solutin #2 (Optimized Dynamic Programming)
+    // Time Complexity: O(n)
+    // Spae Complexity: O(1)
     public int rob(int[] nums) {
-        if (nums.length == 1)
-            return nums[0];
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
+        int N = nums.length;
 
-        int max = Math.max(nums[0], nums[1]);
-        int[] dp = new int[nums.length];
-
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-
-        for (int i = 2; i < nums.length; i++) {
-            for (int j = 0; j < i - 1; j++) {
-                int curSum = dp[j] + nums[i];
-                if (curSum > dp[i]) {
-                    dp[i] = curSum;
-                    max = Math.max(max, curSum);
-                }
-            }
+        // Special handling for empty array case.
+        if (N == 0) {
+            return 0;
         }
 
-        return max;
+        int robNext, robNextPlusOne;
+
+        // Base case initializations.
+        robNextPlusOne = 0;
+        robNext = nums[N - 1];
+
+        // DP table calculations. Note: we are not using any
+        // table here for storing values. Just using two
+        // variables will suffice.
+        for (int i = N - 2; i >= 0; --i) {
+            // Same as the recursive solution.
+            int current = Math.max(robNext, robNextPlusOne + nums[i]);
+
+            // Update the variables
+            robNextPlusOne = robNext;
+            robNext = current;
+        }
+
+        return robNext;
     }
 }
