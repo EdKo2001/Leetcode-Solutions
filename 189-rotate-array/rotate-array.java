72e1db06d9799dc4918f43853c34433f9335d505
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-11-01 11:02:15 -0500

    Time: 1 ms (48.88%) | Memory: 57.5 MB (32.69%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,33 +1,35 @@
 class Solution {
-    // public void rotate(int[] nums, int k) {
-    //     int n = nums.length; // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
-    //     k = k % n;
-    //     int[] rotated = new int[n];
-
-    //     for (int i = 0; i < n; i++) {
-    //         rotated[(i + k) % n] = nums[i];
-    //     }
-
-    //     for (int i = 0; i < n; i++) {
-    //         nums[i] = rotated[i];
-    //     }        
-    // }
-
     public void rotate(int[] nums, int k) {
-        k %= nums.length; // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
+        int n = nums.length; // if k is greater than nums.length then one cycle is completed that means it
+                             // will remain the same and we have to remainder shifts
+        k = k % n;
+        int[] rotated = new int[n];
 
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotated[i];
+        }
     }
 
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
-        }
-    }    
+    // public void rotate(int[] nums, int k) {
+    // k %= nums.length; // if k is greater than nums.length then one cycle is
+    // completed that means it will remain the same and we have to remainder shifts
+
+    // reverse(nums, 0, nums.length - 1);
+    // reverse(nums, 0, k - 1);
+    // reverse(nums, k, nums.length - 1);
+    // }
+
+    // private void reverse(int[] nums, int left, int right) {
+    // while (left < right) {
+    // int temp = nums[left];
+    // nums[left] = nums[right];
+    // nums[right] = temp;
+    // left++;
+    // right--;
+    // }
+    // }
 }
