47763aa0f2d38949b0dabbea218ec905bc23fb6b
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-11-01 21:42:10 -0500

    Time: 1 ms (96.81%) | Memory: 42 MB (37.29%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,19 +1,27 @@
 class Solution {
+    // public boolean isCircularSentence(String sentence) {
+    //     char start = ' ';
+    //     char prev = ' ';
+
+    //     for (int i = 0; i < sentence.length(); i++) {
+    //         if (i == 0)
+    //             start = sentence.charAt(i);
+    //         if (i > 0 && sentence.charAt(i - 1) == ' ' && sentence.charAt(i) != prev)
+    //             return false;
+    //         if (i < sentence.length() - 1 && sentence.charAt(i + 1) == ' ')
+    //             prev = sentence.charAt(i);
+    //         if (i == sentence.length() - 1)
+    //             return start == sentence.charAt(i);
+    //     }
+
+    //     return true;
+    // }
+    
     public boolean isCircularSentence(String sentence) {
-        char start = ' ';
-        char prev = ' ';
-
-        for (int i = 0; i < sentence.length(); i++) {
-            if (i == 0)
-                start = sentence.charAt(i);
-            if (i > 0 && sentence.charAt(i - 1) == ' ' && sentence.charAt(i) != prev)
+        for (int i = 0; i < sentence.length(); ++i)
+            if (sentence.charAt(i) == ' ' &&
+                    sentence.charAt(i - 1) != sentence.charAt(i + 1))
                 return false;
-            if (i < sentence.length() - 1 && sentence.charAt(i + 1) == ' ')
-                prev = sentence.charAt(i);
-            if (i == sentence.length() - 1)
-                return start == sentence.charAt(i);
-        }
-
-        return true;
+        return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
     }
 }
