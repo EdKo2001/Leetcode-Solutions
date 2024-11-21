da7b1bcd97dc2764ab782b7931a2aa210fa393c4
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-11-21 15:15:00 -0600

    Time: 5 ms (33.72%) | Memory: 52.1 MB (19.47%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -6,6 +6,7 @@ class Solution {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
                     cnt++;
+                    // dfs(grid, i, j);
                     bfs(grid, i, j);
                 }
             }
@@ -14,15 +15,42 @@ class Solution {
         return cnt;
     }
 
+    // private void dfs(char[][] grid, int i, int j) {
+    //     if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+    //         return;
+
+    //     grid[i][j] = '0';
+
+    //     bfs(grid, i + 1, j);
+    //     bfs(grid, i - 1, j);
+    //     bfs(grid, i, j + 1);
+    //     bfs(grid, i, j - 1);
+    // }
+
     private void bfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
-            return;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { i, j });
 
-        grid[i][j] = '0';
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-        bfs(grid, i + 1, j);
-        bfs(grid, i - 1, j);
-        bfs(grid, i, j + 1);
-        bfs(grid, i, j - 1);
+        while (!queue.isEmpty()) {
+            int[] pair = queue.poll();
+            int row = pair[0];
+            int col = pair[1];
+            grid[i][j] = '0';
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
+                        || grid[newRow][newCol] == '0')
+                    continue;
+
+                queue.offer(new int[] { newRow, newCol });
+                grid[newRow][newCol] = '0';
+
+            }
+        }
     }
 }
