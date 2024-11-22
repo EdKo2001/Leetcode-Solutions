dda999f8e519cc47426e26ac7366ecbc133f6df6
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-11-22 13:56:25 -0600

    Time: 2 ms (9.16%) | Memory: 45.1 MB (68.51%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -5,7 +5,8 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 'X') {
-                    dfs(board, i, j);
+                    // dfs(board, i, j);
+                    bfs(board, i, j);
                     cnt++;
                 }
             }
@@ -14,14 +15,40 @@ class Solution {
         return cnt;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 'X') {
-            return;
+    // private void dfs(char[][] grid, int i, int j) {
+    // if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j]
+    // != 'X') {
+    // return;
+    // }
+
+    // grid[i][j] = '.';
+
+    // dfs(grid, i, j + 1);
+    // dfs(grid, i + 1, j);
+    // }
+
+    private void bfs(char[][] grid, int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { i, j });
+
+        while (!queue.isEmpty()) {
+            int[] location = queue.poll();
+            int row = location[0];
+            int col = location[1];
+
+            grid[row][col] = '.';
+
+            int[][] directions = { { 0, 1 }, { 1, 0 } };
+            for (int[] direction : directions) {
+                int newRow = row + direction[0];
+                int newCol = col + direction[1];
+
+                if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
+                        || grid[newRow][newCol] != 'X')
+                    continue;
+
+                queue.offer(new int[] { newRow, newCol });
+            }
         }
-
-        grid[i][j] = '.';
-
-        dfs(grid, i, j + 1);
-        dfs(grid, i + 1, j);
     }
 }
