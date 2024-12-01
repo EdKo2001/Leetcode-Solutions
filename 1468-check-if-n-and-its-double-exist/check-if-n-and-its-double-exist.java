7961802f3d6c7bf88e033854e0c169eee74e307d
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-11-30 20:18:14 -0600

    Time: 2 ms (93.52%) | Memory: 43.6 MB (10.97%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,14 +1,12 @@
 class Solution {
-    // The time complexity remains O(n), where n is the length of the input array, as we iterate through the array once and use constant time operations to check and add to the set.
-    // The space complexity is O(n) in the worst case, where all elements are distinct and stored in the set.
     public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Double> seen = new HashSet<>();
 
         for (int num : arr) {
-            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+            if (seen.contains((double) num * 2) || seen.contains((double) num / 2)) {
                 return true;
             }
-            set.add(num);
+            seen.add((double) num);
         }
 
         return false;
