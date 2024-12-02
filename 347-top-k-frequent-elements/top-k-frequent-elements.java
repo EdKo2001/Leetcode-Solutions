bbdcad7c3b1a9708058287f641eee22af36afb07
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-01 22:21:19 -0600

    Time: 14 ms (58.35%) | Memory: 48.8 MB (34.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+        
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
