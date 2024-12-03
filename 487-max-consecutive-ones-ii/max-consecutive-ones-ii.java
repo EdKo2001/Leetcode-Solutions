3534528e88589216ce03d0888f130df202f511f6
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-03 11:15:00 -0600

    Time: 4 ms (33.50%) | Memory: 45.7 MB (34.04%) - LeetSync

--- 487-max-consecutive-ones-ii/max-consecutive-ones-ii.java
+++ 487-max-consecutive-ones-ii/max-consecutive-ones-ii.java
@@ -1,32 +1,28 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int longestSequence = 0;
-        int left = 0;
-        int right = 0;
-        int numZeroes = 0;
+        int left = 0; // Start of the current window
+        int right = 0; // End of the current window
+        int zeroCount = 0; // Count of zeros in the current window
+        int maxCount = 0; // Maximum length of consecutive 1's with at most one zero flipped
 
-        // While our window is in bounds
         while (right < nums.length) {
-
-            // Increase numZeroes if the rightmost element is 0
             if (nums[right] == 0) {
-                numZeroes++;
+                zeroCount++;
             }
 
-            //If our window is invalid, contract our window
-            while (numZeroes == 2) {
+            // If we have more than one zero in the window, move the left pointer
+            while (zeroCount > 1) {
                 if (nums[left] == 0) {
-                    numZeroes--;
+                    zeroCount--;
                 }
                 left++;
             }
 
-            // Update our longest sequence answer
-            longestSequence = Math.max(longestSequence, right - left + 1);
-
-            // Expand our window
+            // Calculate the length of the current window
+            maxCount = Math.max(maxCount, right - left + 1);
             right++;
         }
-        return longestSequence;
+
+        return maxCount;
     }
 }
