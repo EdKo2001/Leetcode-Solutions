a4d9cd67a7e721a8dbeb01cbd5076596736be698
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-09 23:15:32 -0600

    Time: 5 ms (76.19%) | Memory: 45 MB (49.81%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,26 +1,74 @@
 class Solution {
+    // Brute-Force Approach
+    // public int maximumLength(String s) {
+    //     int n = s.length();
+    //     HashMap<String, Integer> substringCount = new HashMap<>();
+    //     int maxLength = -1;
+
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i + 1; j <= n; j++) {
+    //             String sub = s.substring(i, j);
+
+    //             if (isSpecial(sub)) {
+    //                 substringCount.put(sub, substringCount.getOrDefault(sub, 0) + 1);
+
+    //                 if (substringCount.get(sub) >= 3) {
+    //                     maxLength = Math.max(maxLength, sub.length());
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return maxLength;
+    // }
+
+    // private boolean isSpecial(String str) {
+    //     char firstChar = str.charAt(0);
+    //     for (int i = 1; i < str.length(); i++) {
+    //         if (str.charAt(i) != firstChar) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
     public int maximumLength(String s) {
-        int n = s.length();
-        HashMap<String, Integer> substringCount = new HashMap<>();
+ int n = s.length();
+        int left = 1, right = n;
         int maxLength = -1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                String sub = s.substring(i, j);
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-                if (isSpecial(sub)) {
-                    substringCount.put(sub, substringCount.getOrDefault(sub, 0) + 1);
-
-                    if (substringCount.get(sub) >= 3) {
-                        maxLength = Math.max(maxLength, sub.length());
-                    }
-                }
+            if (canFindSpecialSubstring(s, mid)) {
+                maxLength = mid;
+                left = mid + 1; // Try longer substrings
+            } else {
+                right = mid - 1; // Try shorter substrings
             }
         }
 
         return maxLength;
     }
+    
+    // Check if there exists a special substring of given length that appears at least 3 times
+    private boolean canFindSpecialSubstring(String s, int length) {
+        HashMap<String, Integer> substringCount = new HashMap<>();
 
+        for (int i = 0; i <= s.length() - length; i++) {
+            String sub = s.substring(i, i + length);
+            if (isSpecial(sub)) {
+                substringCount.put(sub, substringCount.getOrDefault(sub, 0) + 1);
+                if (substringCount.get(sub) >= 3) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // Check if a string is special (all characters are the same)
     private boolean isSpecial(String str) {
         char firstChar = str.charAt(0);
         for (int i = 1; i < str.length(); i++) {
