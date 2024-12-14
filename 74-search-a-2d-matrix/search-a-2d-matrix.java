426cf9ab754bf8946923def80467f879c87556c0
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-13 20:37:51 -0600

    Time: 0 ms (100.00%) | Memory: 42 MB (74.35%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,22 +1,41 @@
 class Solution {
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     for (int i = 0; i < matrix.length; i++) {
+    //         if (binarySearch(matrix, i, target, 0, matrix[i].length - 1))
+    //             return true;
+    //     }
+
+    //     return false;
+    // }
+
+    // private boolean binarySearch(int[][] matrix, int row, int target, int left, int right) {
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
+    //         if (matrix[row][mid] == target)
+    //             return true;
+    //         else if (matrix[row][mid] > target)
+    //             right = mid - 1;
+    //         else
+    //             left = mid + 1;
+    //     }
+
+    //     return false;
+    // }
+
+    // Top-Right Search
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            if (binarySearch(matrix, i, target, 0, matrix[i].length - 1))
-                return true;
-        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        return false;
-    }
-
-    private boolean binarySearch(int[][] matrix, int row, int target, int left, int right) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (matrix[row][mid] == target)
+        int row = 0, col = cols - 1; // Start from the top-right corner
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
                 return true;
-            else if (matrix[row][mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
+            } else if (matrix[row][col] > target) {
+                col--; // Move left
+            } else {
+                row++; // Move down
+            }
         }
 
         return false;
