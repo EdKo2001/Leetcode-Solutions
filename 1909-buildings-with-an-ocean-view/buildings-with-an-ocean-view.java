4a11ccbbb8d2018bc9155df336a8530ea990786b
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-18 14:30:43 -0600

    Time: 9 ms (42.26%) | Memory: 61.5 MB (14.28%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -1,22 +1,36 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
-        int n = heights.length;
+        List<Integer> result = new ArrayList<>();
+        int maxFromRight = 0;
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        int maxSeen = 0;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (heights[i] > maxSeen) {
-                minHeap.offer(i);
-                maxSeen = heights[i];
+        for (int i = heights.length - 1; i >= 0; i--) {
+            if (heights[i] > maxFromRight) {
+                result.add(i); 
+                maxFromRight = heights[i]; 
             }
         }
 
-        int[] res = new int[minHeap.size()];
-        int resIndex = 0;
-        while (!minHeap.isEmpty()) {
-            res[resIndex++] = minHeap.poll();
-        }
-        return res;
+        Collections.reverse(result);
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+
+        // int n = heights.length;
+
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // int maxSeen = 0;
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     if (heights[i] > maxSeen) {
+        //         minHeap.offer(i);
+        //         maxSeen = heights[i];
+        //     }
+        // }
+
+        // int[] res = new int[minHeap.size()];
+        // int resIndex = 0;
+        // while (!minHeap.isEmpty()) {
+        //     res[resIndex++] = minHeap.poll();
+        // }
+        // return res;
     }
 }
