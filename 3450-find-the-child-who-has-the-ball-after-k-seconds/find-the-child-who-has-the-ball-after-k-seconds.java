02c7f9db26452767b88bc45c4cec7848879db3f2
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-19 21:38:35 -0600

    Time: 0 ms (100.00%) | Memory: 40.8 MB (59.05%) - LeetSync

--- 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.java
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.java
@@ -1,14 +1,29 @@
 class Solution {
     public int numberOfChild(int n, int k) {
-        // Find the effective position in the oscillation period
-        int period = 2 * (n - 1);
-        int effectivePosition = k % period;
+        int ans = 0;
+        boolean plus = true;
 
-        // If within the ascending part of the period
-        if (effectivePosition <= n - 1) {
-            return effectivePosition;
+        // Calculate the oscillation period
+        int period = 2 * (n - 1);
+
+        // Find the effective position in the current oscillation period
+        int effectiveK = k % period;
+
+        // Iterate only up to the effective position
+        for (int i = 1; i <= effectiveK; i++) {
+            if (plus) {
+                ans++;
+                if (ans == n - 1) {
+                    plus = false;
+                }
+            } else {
+                ans--;
+                if (ans == 0) {
+                    plus = true;
+                }
+            }
         }
-        // If within the descending part of the period
-        return period - effectivePosition;
+
+        return ans;
     }
 }
