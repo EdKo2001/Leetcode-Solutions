6e22d0cb0ad685059dbe4c4fbb9ecffd8351b49d
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-19 18:19:10 -0600

    Time: 0 ms (100.00%) | Memory: 47.8 MB (96.31%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -15,8 +15,8 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        // dfs(root.left, root.right, 1);
-        bfs(root);
+        dfs(root.left, root.right, 1);
+        // bfs(root);
         return root;
     }
 
@@ -36,39 +36,39 @@ class Solution {
         dfs(left.right, right.left, depth + 1);
     }
 
-    private void bfs(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+    // private void bfs(TreeNode root) {
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
 
-        int depth = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<TreeNode> currentLevel = new ArrayList<>();
+    //     int depth = 0;
+    //     while (!queue.isEmpty()) {
+    //         int size = queue.size();
+    //         List<TreeNode> currentLevel = new ArrayList<>();
 
-            // Collect nodes of the current level
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                currentLevel.add(node);
+    //         // Collect nodes of the current level
+    //         for (int i = 0; i < size; i++) {
+    //             TreeNode node = queue.poll();
+    //             currentLevel.add(node);
 
-                if (node.left != null)
-                    queue.offer(node.left);
-                if (node.right != null)
-                    queue.offer(node.right);
-            }
+    //             if (node.left != null)
+    //                 queue.offer(node.left);
+    //             if (node.right != null)
+    //                 queue.offer(node.right);
+    //         }
 
-            // Reverse values if at an odd level
-            if (depth % 2 == 1) {
-                int left = 0, right = currentLevel.size() - 1;
-                while (left < right) {
-                    int temp = currentLevel.get(left).val;
-                    currentLevel.get(left).val = currentLevel.get(right).val;
-                    currentLevel.get(right).val = temp;
-                    left++;
-                    right--;
-                }
-            }
+    //         // Reverse values if at an odd level
+    //         if (depth % 2 == 1) {
+    //             int left = 0, right = currentLevel.size() - 1;
+    //             while (left < right) {
+    //                 int temp = currentLevel.get(left).val;
+    //                 currentLevel.get(left).val = currentLevel.get(right).val;
+    //                 currentLevel.get(right).val = temp;
+    //                 left++;
+    //                 right--;
+    //             }
+    //         }
 
-            depth++;
-        }
-    }
+    //         depth++;
+    //     }
+    // }
 }
