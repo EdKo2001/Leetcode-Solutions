65670c03ba69e2cae1e9fe0d3674f682589b39d6
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2024-12-19 21:28:01 -0600

    Time: 31 ms (64.09%) | Memory: 58.3 MB (85.46%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -6,10 +6,12 @@ class Solution {
         Deque<Integer> deque = new ArrayDeque<>();
 
         for (int i = 0; i < nums.length; i++) {
+            // Remove indices that are out of the sliding window
             if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
                 deque.pollFirst();
             }
 
+            // Remove elements smaller than the current element from the deque
             while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
                 deque.pollLast();
             }
