34b60f8b711f0405e3d710b2bf7469e33fd1ea1a
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-17 11:53:36 -0600

    Time: 0 ms (100.00%) | Memory: 51.8 MB (8.69%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.js
+++ 257-binary-tree-paths/binary-tree-paths.js
@@ -10,29 +10,19 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
-var binaryTreePaths = function(root) {
-    const result = [];
-    
-    // Helper function for backtracking
-    const backtrack = (node, path) => {
-        if (!node) return; // Base case: if the node is null, stop further processing
-        
-        // Add the current node's value to the path
-        path.push(node.val);
-        
-        // If it's a leaf node, add the path to the result
+var binaryTreePaths = function (root) {
+    const res = [];
+
+    const backtracking = (node, arr) => {
         if (!node.left && !node.right) {
-            result.push(path.join("->"));
+            arr.push(node.val);
+            res.push(arr.join("->"));
         } else {
-            // Recursively explore left and right subtrees
-            backtrack(node.left, path);
-            backtrack(node.right, path);
+            node.left && backtracking(node.left, [...arr, node.val]);
+            node.right && backtracking(node.right, [...arr, node.val]);
         }
-        
-        // Backtrack: remove the current node's value from the path
-        path.pop();
-    };
-    
-    backtrack(root, []);
-    return result;
+    }
+
+    backtracking(root, []);
+    return res;
 };
