8288959f4102f756029373068700482b432e73ca
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-20 11:23:15 -0600

    Time: 1 ms (46.34%) | Memory: 51.6 MB (87.47%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    let max = 0;
+    max = Math.max(max, traverse(root, 0));
+    return max;
+};
+
+const traverse = (node, depth) => {
+    if (!node) return depth;
+
+    let max = 0;
+
+    max = Math.max(max, traverse(node.left, depth + 1));
+    max = Math.max(max, traverse(node.right, depth + 1));
+
+    return max;
+}
