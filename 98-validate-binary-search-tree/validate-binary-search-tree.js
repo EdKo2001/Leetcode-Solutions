ea874cc152735c68584ab89de603cb21aaa75f97
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-20 14:39:51 -0600

    Time: 0 ms (100.00%) | Memory: 53.3 MB (48.77%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val);
+ *     this.left = (left === undefined ? null : left);
+ *     this.right = (right === undefined ? null : right);
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    return traverse(root, -Infinity, Infinity);
+};
+
+const traverse = (node, min, max) => {
+    if (!node) return true;
+
+    if (node.val <= min || node.val >= max) return false;
+
+    return (
+        traverse(node.left, min, node.val) &&
+        traverse(node.right, node.val, max)
+    );
+};
