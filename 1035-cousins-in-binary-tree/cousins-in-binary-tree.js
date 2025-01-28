bdf0bb4acb5205fa229859b69f595855a8775296
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-28 11:16:25 -0600

    Time: 0 ms (100.00%) | Memory: 51.3 MB (80.40%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.js
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.js
@@ -1,3 +1,48 @@
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val === undefined ? 0 : val);
+//  *     this.left = (left === undefined ? null : left);
+//  *     this.right = (right === undefined ? null : right);
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @param {number} x
+//  * @param {number} y
+//  * @return {boolean}
+//  */
+// var isCousins = function (root, x, y) {
+//     return bfs(root, x, y);
+// };
+
+// const bfs = (root, x, y) => {
+//     const queue = [{ node: root, parent: null }];
+
+//     while (queue.length > 0) {
+//         const lvlLength = queue.length;
+//         let xParent = null;
+//         let yParent = null;
+
+//         for (let i = 0; i < lvlLength; i++) {
+//             const { node, parent } = queue.shift();
+
+//             if (node.val === x) xParent = parent;
+//             if (node.val === y) yParent = parent;
+
+//             if (xParent && yParent) return xParent !== yParent;
+
+//             if (node.left) queue.push({ node: node.left, parent: node });
+//             if (node.right) queue.push({ node: node.right, parent: node });
+//         }
+
+
+//         if ((xParent && !yParent) || (!xParent && yParent)) return false;
+//     }
+
+//     return false;
+// };
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -13,32 +58,30 @@
  * @return {boolean}
  */
 var isCousins = function (root, x, y) {
-    return bfs(root, x, y);
-};
+    let xDepth = -1, yDepth = -1;
+    let xParent = null, yParent = null;
 
-const bfs = (root, x, y) => {
-    const queue = [{ node: root, parent: null }];
+    const dfs = (node, depth, parent) => {
+        if (!node) return;
 
-    while (queue.length > 0) {
-        const lvlLength = queue.length;
-        let xParent = null;
-        let yParent = null;
-
-        for (let i = 0; i < lvlLength; i++) {
-            const { node, parent } = queue.shift();
-
-            if (node.val === x) xParent = parent;
-            if (node.val === y) yParent = parent;
-
-            if (xParent && yParent) return xParent !== yParent;
-
-            if (node.left) queue.push({ node: node.left, parent: node });
-            if (node.right) queue.push({ node: node.right, parent: node });
+        // If we found x, update its depth and parent
+        if (node.val === x) {
+            xDepth = depth;
+            xParent = parent;
+        }
+        // If we found y, update its depth and parent
+        if (node.val === y) {
+            yDepth = depth;
+            yParent = parent;
         }
 
+        dfs(node.left, depth + 1, node);
+        dfs(node.right, depth + 1, node);
+    };
 
-        if ((xParent && !yParent) || (!xParent && yParent)) return false;
-    }
+    // Start DFS from the root node
+    dfs(root, 0, null);
 
-    return false;
+    // Two nodes are cousins if they have the same depth but different parents
+    return xDepth === yDepth && xParent !== yParent;
 };
