35a6f33804f883e5de9d665460b67aad2052b771
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-28 19:08:28 -0600

    Time: 1 ms (89.66%) | Memory: 52.7 MB (64.65%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function (n, edges) {
+    if (n - 1 !== edges.length) return false;
+
+    const adjList = {};
+    for (let i = 0; i < n; i++) adjList[i] = [];
+
+    for (let [start, end] of edges) {
+        adjList[start].push(end);
+        adjList[end].push(start);
+    }
+
+    const seen = new Set();
+
+    const dfs = (node, parent) => {
+        seen.add(node);
+        for (const neighbor of adjList[node]) {
+            if (neighbor === parent) continue;
+            if (seen.has(neighbor)) return false;
+            if (!dfs(neighbor, node)) return false;
+        }
+        return true;
+    };
+
+    if (!dfs(0, -1)) return false;
+
+    return seen.size === n;
+};
