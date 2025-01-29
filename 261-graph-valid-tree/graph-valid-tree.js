1a1d95c1f00b497c4967468e3a6f1ccb0d9272f1
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-28 19:10:07 -0600

    Time: 3 ms (63.79%) | Memory: 52.9 MB (62.36%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.js
+++ 261-graph-valid-tree/graph-valid-tree.js
@@ -1,3 +1,36 @@
+// /**
+//  * @param {number} n
+//  * @param {number[][]} edges
+//  * @return {boolean}
+//  */
+// var validTree = function (n, edges) {
+//     if (n - 1 !== edges.length) return false;
+
+//     const adjList = {};
+//     for (let i = 0; i < n; i++) adjList[i] = [];
+
+//     for (let [start, end] of edges) {
+//         adjList[start].push(end);
+//         adjList[end].push(start);
+//     }
+
+//     const seen = new Set();
+
+//     const dfs = (node, parent) => {
+//         seen.add(node);
+//         for (const neighbor of adjList[node]) {
+//             if (neighbor === parent) continue;
+//             if (seen.has(neighbor)) return false;
+//             if (!dfs(neighbor, node)) return false;
+//         }
+//         return true;
+//     };
+
+//     if (!dfs(0, -1)) return false;
+
+//     return seen.size === n;
+// };
+
 /**
  * @param {number} n
  * @param {number[][]} edges
@@ -15,18 +48,18 @@ var validTree = function (n, edges) {
     }
 
     const seen = new Set();
+    const queue = [[0, -1]];
 
-    const dfs = (node, parent) => {
+    while (queue.length) {
+        const [node, parent] = queue.shift();
         seen.add(node);
+
         for (const neighbor of adjList[node]) {
             if (neighbor === parent) continue;
             if (seen.has(neighbor)) return false;
-            if (!dfs(neighbor, node)) return false;
+            queue.push([neighbor, node]);
         }
-        return true;
-    };
-
-    if (!dfs(0, -1)) return false;
+    }
 
     return seen.size === n;
 };
