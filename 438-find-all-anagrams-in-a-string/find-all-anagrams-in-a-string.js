bfd94407435ecbe7cb97fe8ff2e01a928cf3e1ec
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-29 19:19:42 -0600

    Time: 63 ms (32.70%) | Memory: 57 MB (50.82%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function (s, p) {
+    const anagramTable = {};
+    for (const char of p) {
+        anagramTable[char] = (anagramTable[char] || 0) + 1;
+    }
+
+    const res = [];
+    const windowTable = {};
+    let left = 0, right = 0;
+
+    while (right < s.length) {
+        windowTable[s[right]] = (windowTable[s[right]] || 0) + 1;
+
+        if (right - left + 1 === p.length) {
+            if (matchAnagramTables(anagramTable, windowTable)) {
+                res.push(left);
+            }
+            windowTable[s[left]]--;
+            // if (windowTable[s[left]] === 0) delete windowTable[s[left]];
+            left++;
+        }
+
+        right++;
+    }
+
+    return res;
+};
+
+const matchAnagramTables = (table1, table2) => {
+    return Object.keys(table1).every(key => table1[key] === table2[key]);
+};
