14c63f19331cd4cb3f5d6a1f8ca4e1804fe300c0
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-31 19:24:49 -0600

    Time: 222 ms (7.69%) | Memory: 79.8 MB (23.08%) - LeetSync

--- 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.js
+++ 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.js
@@ -4,44 +4,44 @@
  * @return {number[]}
  */
 var distinctNumbers = function (nums, k) {
-    const freqMap = new Map();
-    const res = new Array(nums.length - k + 1);
-
-    for (let i = 0; i < k; i++) {
-        freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
-    }
-    res[0] = freqMap.size;
-
-    for (let i = k; i < nums.length; i++) {
-        freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
-
-        const oldElement = nums[i - k];
-        freqMap.set(oldElement, freqMap.get(oldElement) - 1);
-        if (freqMap.get(oldElement) === 0) {
-            freqMap.delete(oldElement);
-        }
-
-        res[i - k + 1] = freqMap.size;
-    }
-
-    return res;
-
     // const freqMap = new Map();
     // const res = new Array(nums.length - k + 1);
 
-    // for (let i = 0; i < nums.length; i++) {
+    // for (let i = 0; i < k; i++) {
+    //     freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
+    // }
+    // res[0] = freqMap.size;
+
+    // for (let i = k; i < nums.length; i++) {
     //     freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
 
-    //     if (i >= k - 1) {
-    //         res[i - k + 1] = freqMap.size;
-
-    //         const oldElement = nums[i - k + 1];
-    //         freqMap.set(oldElement, freqMap.get(oldElement) - 1);
-    //         if (freqMap.get(oldElement) === 0) {
-    //             freqMap.delete(oldElement);
-    //         }
+    //     const oldElement = nums[i - k];
+    //     freqMap.set(oldElement, freqMap.get(oldElement) - 1);
+    //     if (freqMap.get(oldElement) === 0) {
+    //         freqMap.delete(oldElement);
     //     }
+
+    //     res[i - k + 1] = freqMap.size;
     // }
 
     // return res;
+
+    const freqMap = new Map();
+    const res = new Array(nums.length - k + 1);
+
+    for (let i = 0; i < nums.length; i++) {
+        freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
+
+        if (i >= k - 1) {
+            res[i - k + 1] = freqMap.size;
+
+            const oldElement = nums[i - k + 1];
+            freqMap.set(oldElement, freqMap.get(oldElement) - 1);
+            if (freqMap.get(oldElement) === 0) {
+                freqMap.delete(oldElement);
+            }
+        }
+    }
+
+    return res;
 };
