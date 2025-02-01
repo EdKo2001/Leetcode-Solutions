97e2c60768bf0f6892b2f440f6e2f9dd2a937dff
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-01-31 18:34:26 -0600

    Time: 1 ms (64.19%) | Memory: 51.7 MB (48.37%) - LeetSync

--- 3429-special-array-i/special-array-i.js
+++ 3429-special-array-i/special-array-i.js
@@ -3,10 +3,19 @@
  * @return {boolean}
  */
 var isArraySpecial = function (nums) {
-    let parity = nums[0] % 2;
+    // if (nums.length <= 1) return true; 
+    // let parity = nums[0] % 2;
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[i] % 2 === parity) return false;
+    //     parity = 1 - parity;
+    // }
+    // return true;
+
+    if (nums.length <= 1) return true;
+
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] % 2 === parity) return false;
-        parity = 1 - parity;
+        if (nums[i] % 2 === nums[i - 1] % 2) return false;
     }
+
     return true;
 };
