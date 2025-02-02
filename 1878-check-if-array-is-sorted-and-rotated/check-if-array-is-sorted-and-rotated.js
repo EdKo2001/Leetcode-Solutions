a20d4a102ecbdbf2b507e4edd4075a94ade3606c
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-01 20:25:54 -0600

    Time: 0 ms (100.00%) | Memory: 48.8 MB (78.01%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function (nums) {
+    let dropCount = 0;
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            dropCount++;
+        }
+    }
+
+    return dropCount <= 1;
+};
