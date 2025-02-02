9f3c06605bb286be27c08ead4ebdef517ec9c9d3
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-02 11:11:56 -0600

    Time: 80 ms (17.69%) | Memory: 79.7 MB (23.15%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.js
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.js
@@ -4,27 +4,50 @@
  * @return {number}
  */
 var maximumSubarraySum = function (nums, k) {
+    // Two Loops
+    // const freqMap = new Map();
+    // let maxSum = 0;
+    // let currSum = 0;
+
+    // for (let i = 0; i < k; i++) {
+    //     const num = nums[i];
+    //     freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    //     currSum += num;
+    // }
+
+    // if (freqMap.size === k) maxSum = Math.max(maxSum, currSum);
+
+    // for (let i = k; i < nums.length; i++) {
+    //     const num = nums[i];
+    //     freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    //     currSum += num;
+
+    //     const old = nums[i - k];
+    //     freqMap.set(old, (freqMap.get(old) || 0) - 1);
+    //     if (freqMap.get(old) === 0) freqMap.delete(old);
+    //     currSum -= old;
+
+    //     if (freqMap.size === k) maxSum = Math.max(maxSum, currSum);
+    // }
+
+    // return maxSum;
+
+    // One Loop
     const freqMap = new Map();
     let maxSum = 0;
     let currSum = 0;
 
-    for (let i = 0; i < k; i++) {
-        const num = nums[i];
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
-        currSum += num;
-    }
-
-    if (freqMap.size === k) maxSum = Math.max(maxSum, currSum);
-
-    for (let i = k; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
         freqMap.set(num, (freqMap.get(num) || 0) + 1);
         currSum += num;
 
-        const old = nums[i - k];
-        freqMap.set(old, (freqMap.get(old) || 0) - 1);
-        if (freqMap.get(old) === 0) freqMap.delete(old);
-        currSum -= old;
+        if (i >= k) {
+            const old = nums[i - k];
+            freqMap.set(old, (freqMap.get(old) || 0) - 1);
+            if (freqMap.get(old) === 0) freqMap.delete(old);
+            currSum -= old;
+        }
 
         if (freqMap.size === k) maxSum = Math.max(maxSum, currSum);
     }
