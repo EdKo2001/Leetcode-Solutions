f595921c6c9a7d5705612ac1e60c2177e4679b58
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-07 16:44:55 -0600

    Time: 44 ms (21.85%) | Memory: 51 MB (69.46%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // Use a PriorityQueue that orders points based on their distance from the
-        // origin
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-                (p1, p2) -> Double.compare(distance(p1), distance(p2)));
+        Queue<Integer> maxHeap = new PriorityQueue<>(
+                (a, b) -> (points[b][0] * points[b][0] + points[b][1] * points[b][1])
+                        - (points[a][0] * points[a][0] + points[a][1] * points[a][1]));
 
-        // Add all points to the heap
-        for (int[] point : points) {
-            minHeap.offer(point);
+        for (int i = 0; i < points.length; i++) {
+            maxHeap.offer(i);
+
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
         }
 
-        // Collect the k closest points
         int[][] res = new int[k][2];
+        int resIdx = 0;
         for (int i = 0; i < k; i++) {
-            res[i] = minHeap.poll();
+            res[resIdx++] = points[maxHeap.poll()];
         }
-
         return res;
-    }
-
-    // Helper method to calculate the distance from the origin
-    private double distance(int[] point) {
-        return Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
     }
 }
