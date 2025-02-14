f5330a884e5d4c426cad66bc3df5a5f8d48e520e
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-14 08:51:36 -0600

    Time: 5 ms (67.29%) | Memory: 50.8 MB (88.85%) - LeetSync

--- 38-count-and-say/count-and-say.js
+++ 38-count-and-say/count-and-say.js
@@ -2,58 +2,54 @@
  * @param {number} n
  * @return {string}
  */
-// var countAndSay = function (n) {
-//     let stack = [];
-
-//     for (let i = 1; i <= n; i++) {
-//         if (i == 1) {
-//             stack.push(1);
-//             continue;
-//         }
-//         const temp = [];
-//         let prevNumber = stack[0];
-//         let cnt = 1;
-//         for (let j = 1; j < stack.length; j++) {
-//             if (prevNumber != stack[j]) {
-//                 temp.push(cnt);
-//                 temp.push(prevNumber);
-//                 prevNumber = stack[j];
-//                 cnt = 1;
-//             } else {
-//                 cnt++;
-//             }
-//         }
-//         temp.push(cnt);
-//         temp.push(prevNumber);
-//         stack = temp;
-//     }
-
-//     return stack.join('');
-// };
-
-/**
- * @param {number} n
- * @return {string}
- */
 var countAndSay = function (n) {
-    let prev = "1";
+    let stack = [];
 
-    for (let i = 2; i <= n; i++) {
-        let count = 1;
-        let result = "";
-
-        for (let j = 1; j < prev.length; j++) {
-            if (prev[j] === prev[j - 1]) {
-                count++;
+    for (let i = 1; i <= n; i++) {
+        if (i == 1) {
+            stack.push(1);
+            continue;
+        }
+        const temp = [];
+        let prevNumber = stack[0];
+        let cnt = 1;
+        for (let j = 1; j < stack.length; j++) {
+            if (prevNumber != stack[j]) {
+                temp.push(cnt);
+                temp.push(prevNumber);
+                prevNumber = stack[j];
+                cnt = 1;
             } else {
-                result += count + prev[j - 1];
-                count = 1;
+                cnt++;
             }
         }
-
-        result += count + prev[prev.length - 1];
-        prev = result;
+        temp.push(cnt);
+        temp.push(prevNumber);
+        stack = temp;
     }
 
-    return prev;
+    return stack.join('');
 };
+
+// var countAndSay = function (n) {
+//     let prev = "1";
+
+//     for (let i = 2; i <= n; i++) {
+//         let count = 1;
+//         let result = "";
+
+//         for (let j = 1; j < prev.length; j++) {
+//             if (prev[j] === prev[j - 1]) {
+//                 count++;
+//             } else {
+//                 result += count + prev[j - 1];
+//                 count = 1;
+//             }
+//         }
+
+//         result += count + prev[prev.length - 1];
+//         prev = result;
+//     }
+
+//     return prev;
+// };
