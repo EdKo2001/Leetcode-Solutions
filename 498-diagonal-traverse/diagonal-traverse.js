a7e0a86d1f60f96da789d9e4b1cd71808ad72a77
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-15 21:03:46 -0600

    Time: 15 ms (34.85%) | Memory: 61.1 MB (13.47%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.js
+++ 498-diagonal-traverse/diagonal-traverse.js
@@ -25,3 +25,42 @@ var findDiagonalOrder = function (mat) {
 
     return result;
 };
+
+// Optimized Simulation (O(1) Extra Space)
+// var findDiagonalOrder = function (mat) {
+//     if (!mat || !mat.length || !mat[0].length) return [];
+
+//     let rows = mat.length, cols = mat[0].length;
+//     let result = new Array(rows * cols);
+//     let r = 0, c = 0, up = true;
+
+//     for (let i = 0; i < rows * cols; i++) {
+//         result[i] = mat[r][c];
+
+//         if (up) {
+//             if (c === cols - 1) { // Reached right boundary
+//                 r++;
+//                 up = false;
+//             } else if (r === 0) { // Reached top boundary
+//                 c++;
+//                 up = false;
+//             } else {
+//                 r--;
+//                 c++;
+//             }
+//         } else {
+//             if (r === rows - 1) { // Reached bottom boundary
+//                 c++;
+//                 up = true;
+//             } else if (c === 0) { // Reached left boundary
+//                 r++;
+//                 up = true;
+//             } else {
+//                 r++;
+//                 c--;
+//             }
+//         }
+//     }
+
+//     return result;
+// };
