047ca11eb4a1e44e7cc2282107b1748257846634
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-16 00:04:54 -0600

    Time: 0 ms (100.00%) | Memory: 51.5 MB (100.00%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.js
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.js
@@ -4,22 +4,16 @@
  * @return {boolean}
  */
 var hasSpecialSubstring = function (s, k) {
-    let prev = -1;
-    let curr = -1;
     let cnt = 1;
-    for (let i = 0; i < s.length; i++) {
-        if (i === 0) {
-            curr = 0;
-        } else
-            if (s[i] === s[curr]) {
-                cnt++;
-            } else {
-                curr = i;
-                cnt = 1;
-                prev = i - 1;
-            }
 
-        if (cnt === k && prev !== curr && (i === s.length - 1 ? true : s[curr] !== s[i + 1])) return true;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            cnt++;
+        } else {
+            if (cnt === k) return true;
+            cnt = 1;
+        }
     }
-    return false;
+
+    return cnt === k;
 };
