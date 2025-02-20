fc6e0cb5666f3147f250a8acdbfee58c541f994b
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-02-19 23:33:27 -0600

    Time: 8 ms (61.77%) | Memory: 63.7 MB (7.10%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0) return false;
+
+    let originalNumber = x;
+    let reversedNumber = 0;
+
+    while (x > 0) {
+        reversedNumber = reversedNumber * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return reversedNumber === originalNumber;
+};
