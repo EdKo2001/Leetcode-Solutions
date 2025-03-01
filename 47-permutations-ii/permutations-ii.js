7c6306748a8eb362dc6a721b1ed4feb86239bc70
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-01 13:02:45 -0600

    Time: 1 ms (98.35%) | Memory: 58.6 MB (20.00%) - LeetSync

--- 47-permutations-ii/permutations-ii.js
+++ 47-permutations-ii/permutations-ii.js
@@ -3,42 +3,23 @@
  * @return {number[][]}
  */
 var permuteUnique = function (nums) {
-    const res = [];
     const n = nums.length;
-    nums.sort((a, b) => a - b); // Sort the array to handle duplicates
-    const used = Array(n).fill(false); // Track used elements
+    const ans = [];
+    const visited = Array.from({ length: n }, () => false);
+
+    nums.sort((a, b) => a - b);
 
     const backtracking = (arr) => {
-        // Base case: If the permutation is complete
-        if (arr.length === n) {
-            res.push([...arr]); // Push a copy of the current permutation
-            return;
-        }
-
-        // Iterate through all numbers
+        if (arr.length === n) return ans.push([...arr]);
         for (let i = 0; i < n; i++) {
-            // Skip duplicates
-            if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
-
-            // Skip if the number is already used
-            if (used[i]) continue;
-
-            // Mark the number as used
-            used[i] = true;
-
-            // Include the current number in the permutation
+           if (visited[i] || (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1])) continue;
             arr.push(nums[i]);
-
-
-            // Recur with the updated permutation
+            visited[i] = true;
             backtracking(arr);
-
-            // Backtrack: Remove the last number and mark it as unused
             arr.pop();
-            used[i] = false;
+            visited[i] = false;
         }
-    };
-
+    }
     backtracking([]);
-    return res;
+    return ans;
 };
