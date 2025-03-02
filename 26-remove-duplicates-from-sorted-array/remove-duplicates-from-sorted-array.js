60d5c90c34754bc8f1d201345093f4b0c2a6bdb6
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-02 16:05:36 -0600

    Time: 0 ms (100.00%) | Memory: 58.1 MB (9.62%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -1,16 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var removeDuplicates = function(nums) {
-    let i = 0; // Pointer for the unique elements
+var removeDuplicates = function (nums) {
+    let i = 1; // Pointer for the next unique element
 
     for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++;
+        if (nums[j] !== nums[i - 1]) { 
             nums[i] = nums[j];
+            i++;
         }
     }
 
-    return i + 1; // Length of unique elements
+    return i; 
 };
