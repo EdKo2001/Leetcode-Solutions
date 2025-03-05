649787a090ea8f88755fbc1cd8d8eddad789a851
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-04 18:34:21 -0600

    Time: 3 ms (31.58%) | Memory: 54.3 MB (21.05%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.js
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.js
@@ -4,13 +4,13 @@
  */
 var coloredCells = function (n) {
     let numBlueCells = 1;
-    let addend = 4;
+    let added = 4;
 
     // Iterate n - 1 times
     while (--n > 0) {
         // Add and update current multiple of 4
-        numBlueCells += addend;
-        addend += 4;
+        numBlueCells += added;
+        added += 4;
     }
 
     return numBlueCells;
