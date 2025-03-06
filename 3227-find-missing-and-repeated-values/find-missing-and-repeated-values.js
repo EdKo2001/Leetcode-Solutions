3d3ea1490fbb0b6acee6eb7328546f2fc62ab864
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-05 21:48:57 -0600

    Time: 3 ms (90.87%) | Memory: 57.5 MB (20.24%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
@@ -2,18 +2,43 @@
  * @param {number[][]} grid
  * @return {number[]}
  */
-var findMissingAndRepeatedValues = function (grid) {
+var findMissingAndRepeatedValues = function(grid) {
     const n = grid.length * grid[0].length;
-    const seen = new Set();
-    let repeated = -1, sum = 0, expectedSum = (n * (n + 1)) / 2;
+    const seen = Array(n).fill(false);
+    let repeated = -1, missing = -1;
 
-    for (const row of grid) {
-        for (const num of row) {
-            if (seen.has(num)) repeated = num;
-            seen.add(num);
-            sum += num;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            const num = grid[i][j];
+            if (seen[num - 1]) {
+                repeated = num;
+            }
+            seen[num - 1] = true;
         }
     }
 
-    return [repeated, expectedSum - (sum - repeated)];
+    missing = seen.indexOf(false) + 1; // Find the missing number
+
+    return [repeated, missing];
 };
+
+
+// /**
+//  * @param {number[][]} grid
+//  * @return {number[]}
+//  */
+// var findMissingAndRepeatedValues = function (grid) {
+//     const n = grid.length * grid[0].length;
+//     const seen = new Set();
+//     let repeated = -1, sum = 0, expectedSum = (n * (n + 1)) / 2;
+
+//     for (const row of grid) {
+//         for (const num of row) {
+//             if (seen.has(num)) repeated = num;
+//             seen.add(num);
+//             sum += num;
+//         }
+//     }
+
+//     return [repeated, expectedSum - (sum - repeated)];
+// };
