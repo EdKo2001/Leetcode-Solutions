6649b6e350bf5886779adf087cf7fff4695b51cf
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-08 14:42:58 -0600

    Time: 264 ms (32.69%) | Memory: 63.5 MB (40.75%) - LeetSync

--- 127-word-ladder/word-ladder.js
+++ 127-word-ladder/word-ladder.js
@@ -4,81 +4,81 @@
  * @param {string[]} wordList
  * @return {number}
  */
-// var ladderLength = function (beginWord, endWord, wordList) {
-//     const wordSet = new Set(wordList);
 
-//     if (!wordSet.has(endWord)) return 0;
+const aCharCode = "a".charCodeAt(0);
 
-//     const queue = [];
-//     queue.push({ word: beginWord, lvl: 1 });
-
-//     let ans = Infinity;
-
-//     while (queue.length > 0) {
-//         const { word, lvl } = queue.shift();
-//         if (word === endWord) {
-//             ans = Math.min(ans, lvl)
-//             continue;
-//         };
-
-//         const charsOfWord = word.split("");
-//         for (let char = 0; char < charsOfWord.length; char++) {
-//             for (let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
-//                 let temp = charsOfWord[char];
-//                 charsOfWord[char] = String.fromCharCode(i);
-//                 const newWord = charsOfWord.join("");
-//                 if (wordSet.has(newWord)) {
-//                     queue.push({ word: newWord, lvl: lvl + 1 });
-//                     wordSet.delete(newWord);
-//                 }
-//                 charsOfWord[char] = temp;
-//             }
-//         }
-//     }
-
-//     return ans === Infinity ? 0 : ans;
-// };
-
-// Bidirectional BFS
 var ladderLength = function (beginWord, endWord, wordList) {
     const wordSet = new Set(wordList);
     if (!wordSet.has(endWord)) return 0;
 
-    let beginSet = new Set([beginWord]);
-    let endSet = new Set([endWord]);
-    let level = 1;
+    const queue = [{ word: beginWord, lvl: 1 }];
 
-    while (beginSet.size > 0 && endSet.size > 0) {
-        // Always expand the smaller set to optimize performance
-        if (beginSet.size > endSet.size) {
-            [beginSet, endSet] = [endSet, beginSet];
-        }
+    while (queue.length > 0) {
+        const { word, lvl } = queue.shift();
 
-        const nextSet = new Set();
-        for (let word of beginSet) {
-            const wordArr = word.split("");
-            for (let i = 0; i < wordArr.length; i++) {
-                const originalChar = wordArr[i];
+        if (word === endWord) return lvl;
 
-                for (let j = 0; j < 26; j++) {
-                    wordArr[i] = String.fromCharCode(97 + j);
-                    const newWord = wordArr.join("");
+        // Generate all possible words by changing one character at a time
+        for (let i = 0; i < word.length; i++) {
+            const chars = word.split("");
 
-                    if (endSet.has(newWord)) return level + 1; // Found shortest path
+            for (let charCode = 0; charCode < 26; charCode++) {
+                const newChar = String.fromCharCode(charCode + aCharCode);
+                chars[i] = newChar;
+                const newWord = chars.join("");
 
-                    if (wordSet.has(newWord)) {
-                        nextSet.add(newWord);
-                        wordSet.delete(newWord);
-                    }
+                if (wordSet.has(newWord)) {
+                    queue.push({ word: newWord, lvl: lvl + 1 });
+                    wordSet.delete(newWord);
                 }
-
-                wordArr[i] = originalChar; // Restore the original character
             }
         }
-
-        beginSet = nextSet;
-        level++;
     }
 
     return 0;
 };
+
+
+// Bidirectional BFS
+// var ladderLength = function (beginWord, endWord, wordList) {
+//     const wordSet = new Set(wordList);
+//     if (!wordSet.has(endWord)) return 0;
+
+//     let beginSet = new Set([beginWord]);
+//     let endSet = new Set([endWord]);
+//     let level = 1;
+
+//     while (beginSet.size > 0 && endSet.size > 0) {
+//         // Always expand the smaller set to optimize performance
+//         if (beginSet.size > endSet.size) {
+//             [beginSet, endSet] = [endSet, beginSet];
+//         }
+
+//         const nextSet = new Set();
+//         for (let word of beginSet) {
+//             const wordArr = word.split("");
+//             for (let i = 0; i < wordArr.length; i++) {
+//                 const originalChar = wordArr[i];
+
+//                 for (let j = 0; j < 26; j++) {
+//                     wordArr[i] = String.fromCharCode(97 + j);
+//                     const newWord = wordArr.join("");
+
+//                     if (endSet.has(newWord)) return level + 1; // Found shortest path
+
+//                     if (wordSet.has(newWord)) {
+//                         nextSet.add(newWord);
+//                         wordSet.delete(newWord);
+//                     }
+//                 }
+
+//                 wordArr[i] = originalChar; // Restore the original character
+//             }
+//         }
+
+//         beginSet = nextSet;
+//         level++;
+//     }
+
+//     return 0;
+// };
