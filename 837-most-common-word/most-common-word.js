9c9e77f2434ffa2c3ee10ce28907d1f871b81c76
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-08 10:51:58 -0600

    Time: 4 ms (70.08%) | Memory: 57.6 MB (6.43%) - LeetSync

---
+++ 837-most-common-word/most-common-word.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function (paragraph, banned) {
+    const freq = {};
+    const bannedSet = new Set(banned.map(word => word.toLowerCase()));
+
+
+    const normalizedStr = paragraph.toLowerCase().replace(/[^a-z0-9]+/g, ' ');
+
+    let string = "";
+    let maxCount = 0;
+    let ans = "";
+    for (let char of normalizedStr) {
+        if (char === " ") {
+            if (bannedSet.has(string)) {
+                string = "";
+                continue;
+            }
+            freq[string] = (freq[string] || 0) + 1;
+
+            if (freq[string] > maxCount) {
+                maxCount = freq[string];
+                ans = string
+            }
+
+            string = "";
+            continue;
+        }
+        string += char;
+    }
+
+    if (bannedSet.has(string)) return ans;
+    freq[string] = (freq[string] || 0) + 1;
+
+    if (freq[string] > maxCount) {
+        maxCount = freq[string];
+        return string;
+    }
+
+    return ans;
+};
