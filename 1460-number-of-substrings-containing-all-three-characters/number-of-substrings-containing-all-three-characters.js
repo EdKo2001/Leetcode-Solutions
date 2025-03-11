2ab8cfead6a9624dbbdf411120e3ee1ff8624d80
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2025-03-10 19:48:34 -0500

    Time: 42 ms (24.80%) | Memory: 55.4 MB (48.43%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function (s) {
+    let cnt = 0;
+    let start = 0;
+    const currWindow = new Map();
+
+    for (let end = 0; end < s.length; end++) {
+        currWindow.set(s[end], (currWindow.get(s[end]) || 0) + 1);
+
+
+        while (currWindow.get('a') > 0 && currWindow.get('b') > 0 && currWindow.get('c') > 0) {
+            cnt += s.length - end;
+
+            currWindow.set(s[start], currWindow.get(s[start]) - 1);
+            start++;
+        }
+    }
+    return cnt;
+};
